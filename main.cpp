d4fda29e1f1a3e7fba327b56c8322ad8d8494ec8
kepler452b123 <adept5535@gmail.com>
2023-07-17 16:11:21 -0700

    Co-authored-by: Owen Zhang <owenz0523@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include "player.h"
+#include "card.h"
+#include "deck.h"
+#include <iostream>
+#include <list>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+
+
+int main() {
+    Deck gameDeck = Deck();
+    gameDeck.initialize();
+    gameDeck.toString();
+    return 0;
+}
